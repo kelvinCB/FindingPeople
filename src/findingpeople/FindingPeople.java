2371762309158508f6a1a14cd8cdd9d75a66b013
KelvinLearning <kelvinr02@hotmail.com>
2020-04-01 19:09:13 -0400

    P3-Completando todo el Login

---
+++ src/findingpeople/FindingPeople.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package findingpeople;
+
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author USER
+ */
+public class FindingPeople {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+
+        /*Login login = new Login();
+        login.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        login.setVisible(true);*/
+       
+       Container container = new Container();
+       container.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+       container.setVisible(true);
+
+    }
+
+}
